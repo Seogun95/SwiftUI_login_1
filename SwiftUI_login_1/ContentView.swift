4d48653769012784eabd0ef8b5025ebe840abfa3
SeonJungKim <ksj083895@gmail.com>
2021-04-29 20:03:22 +0500

    Initial Commit

---
+++ SwiftUI_login_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_login_1
+//
+//  Created by 김선중 on 2021/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_login_1/SwiftUI_login_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_login_1App.swift
+//  SwiftUI_login_1
+//
+//  Created by 김선중 on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_login_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
